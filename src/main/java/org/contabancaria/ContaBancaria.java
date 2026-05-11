73ce34f69327e669351a164811331cff22631f2f
Lucas. <lucascamposnew@outlook.com.>
2026-05-11 13:10:44 -0300

    fiz a conexão com o banco, a dependencia do mySql no pom.xml e também o DAO do titular

--- src/main/java/org/contabancaria/ContaBancaria.java
+++ src/main/java/org/contabancaria/ContaBancaria.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.contabancaria;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
--- src/main/java/org/contabancaria/Main.java
+++ src/main/java/org/contabancaria/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package org.contabancaria;
 
 import java.util.Scanner;
 
